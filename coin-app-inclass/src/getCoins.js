0092063e1177b3d2357364f2f87869ab19ea676d
ibr <ibr.seckin@gmail.com>
2024-03-13 23:54:58 +0300

    update

--- coin-app-inclass/src/getCoins.js
+++ coin-app-inclass/src/getCoins.js
@@ -1,32 +1,37 @@
 
 //import default => could get any game
 import  displayCoin  from "./displayCoins"; 
+import axios from "axios"
 
 const BASE_URL = "https://api.coinranking.com/v2/";
-const API_KEY = "coinrankinga9aae188e3b80d3ba69940202e8ba01daf51f9c184a5352f";
+// const API_KEY = "";
 const SEARCH_ENDPOINT = "coins";
 
 
 export const getCoinsApi = async(value) => {
     const options = {
-        method:"GET",
+        // headers:{
+        //     "x-access-token": API_KEY,
+        // }
         headers:{
-            "x-access-token": API_KEY,
+            "x-access-token": import.meta.env.VITE_API_KEY,
         }
     }
     const URL = `${BASE_URL}${SEARCH_ENDPOINT}?search=${value}&limit=1`;
-    const response = await fetch(URL,options);
+    const response = await axios(URL,options);
     try{
         if(response.status === 200){
-            const jsonData = await response.json();
-            if(!jsonData.data.coins[0]){
+            console.log(response);
+            // const jsonData = await response.json(); // from fetch
+            if(!response.data.data.coins[0]){
 
                 alert("coin bulunamadi")
             }else{
-                displayCoin(jsonData.data.coins[0]);
+                displayCoin(response.data.data.coins[0]);
             }
         }
     }catch(error){
         console.log(error);
     }
 }
+
